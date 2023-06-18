10d7ead1702342cb2d34d4b5c3816bd5b98184e4
WendyHellena <hellenaadala@gmail.com>
2023-06-18 21:59:15 +0300

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers of base 10 starting from 0
+ *
+ * Description: using the main function
+ * this program prints all aingle digit numbers of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+	for (c = 48; c < 58; c++)
+	putchar(c);
+	putchar('\n');
+
+return (0);
+}
